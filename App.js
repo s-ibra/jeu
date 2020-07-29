bf29fe2b1045150c57fed6cf22af4d61c09ff37a
s-ibra <65419218+s-ibra@users.noreply.github.com>
2020-07-29 15:05:26 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,114 @@
+
+import React, { Component } from 'react'
+import Card from './Card'
+import GuessCount from './GuessCount'
+
+import shuffle from 'lodash.shuffle'
+import HallOfFame, { FAKE_HOF } from './HallOfFame'
+import HighScoreInput from "./HighScoreInput"
+
+import './App.css'
+
+
+const SIDE = 6
+const SYMBOLS = '😀🎉💖🎩🐶🐱🦄🐬🌍🌛🌞💫🍎🍌🍓🍐🍟🍿'
+const VISUAL_PAUSE_MSECS = 750
+
+class App extends Component {
+  state = {
+    cards: this.generateCards(),
+    currentPair: [],
+    guesses: 0,
+    HallOfFame: null,
+    matchedCardIndices: [],
+  }
+  displayHallOfFame = HallOfFame =>{
+    this.setState({ HallOfFame })
+  }
+  generateCards() {
+    const result = []
+    const size = SIDE * SIDE
+    const candidates = shuffle(SYMBOLS)
+    while (result.length < size) {
+      const card = candidates.pop()
+      result.push(card, card)
+    }
+    return shuffle(result)
+  }
+
+  getFeedbackForCard(index) {
+    const { currentPair, matchedCardIndices } = this.state
+    const indexMatched = matchedCardIndices.includes(index)
+
+    if (currentPair.length < 2) {
+      return indexMatched || index === currentPair[0] ? 'visible' : 'hidden'
+    }
+
+    if (currentPair.includes(index)) {
+      return indexMatched ? 'justMatched' : 'justMismatched'
+    }
+
+    return indexMatched ? 'visible' : 'hidden'
+  }
+
+  // Arrow fx for binding
+  handleCardClick = index => {
+    const { currentPair } = this.state
+
+    if (currentPair.length === 2) {
+      return
+    }
+
+    if (currentPair.length === 0) {
+      this.setState({ currentPair: [index] })
+      return
+    }
+
+    this.handleNewPairClosedBy(index)
+  }
+
+  handleNewPairClosedBy(index) {
+    const { cards, currentPair, guesses, matchedCardIndices } = this.state
+
+    const newPair = [currentPair[0], index]
+    const newGuesses = guesses + 1
+    const matched = cards[newPair[0]] === cards[newPair[1]]
+    this.setState({ currentPair: newPair, guesses: newGuesses })
+    if (matched) {
+      this.setState({ matchedCardIndices: [...matchedCardIndices, ...newPair] })
+    }
+    setTimeout(() => this.setState({ currentPair: [] }), VISUAL_PAUSE_MSECS)
+  }
+
+  render() {
+    const { cards, guesses, HallOfFame, matchedCardIndices } = this.state
+    const won = matchedCardIndices.length === 4 //cards.length
+    return (
+      <div className="memory">
+        <GuessCount guesses={guesses} />
+        {cards.map((card, index) => (
+          <Card
+            card={card}
+            feedback={this.getFeedbackForCard(index)}
+            index={index}
+            key={index}
+            onClick={this.handleCardClick}
+          />
+        ))}
+        {
+  won &&
+    (HallOfFame ? (
+      <HallOfFame entries={HallOfFame} />
+    ) : (
+      <HighScoreInput
+      guesses={guesses}
+      onStored={this.displayHallOfFame}
+      />
+    ))}
+
+      </div>
+    )
+  }
+}
+
+export default App
